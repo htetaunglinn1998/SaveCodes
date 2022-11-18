5b43d89ff158a9e0300b60fc20c7a6b5367e33ba
htetaunglinn1998 <htetaung.linn@smilaxglobal.com>
2022-11-18 15:55:08 +0630

    Distinct Added

---
+++ distinct__options.jsx
@@ -0,0 +1,2 @@
+//-----------------------------Distinct Value for dropdown------------------------------
+const uniqueArray = array.filter((a, i) => array.findIndex((s) => a.Value === s.Value) === i)
